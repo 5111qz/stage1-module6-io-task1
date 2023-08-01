52926f4ec5380a7af9ee5a9a51c8c654917ade8b
5111qz <139861805+5111qz@users.noreply.github.com>
2023-08-01 14:16:26 +0200

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> profileData = new HashMap<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            String rawData = "";
+            int i = fileInputStream.read();
+            while(i != -1) {
+                rawData += (char)i;
+                i = fileInputStream.read();
+            }
+            rawData = rawData.replace("\n", ": ");
+            rawData = rawData.substring(0, rawData.length() - 2);
+            fileInputStream.close();
+            String[] rawDataSplit = rawData.split(": ");
+            for(i = 0; i < rawDataSplit.length - 1; i = i + 2) {
+                profileData.put(rawDataSplit[i], rawDataSplit[i+1]);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(profileData.get("Name"),
+                Integer.parseInt(profileData.get("Age")),
+                profileData.get("Email"),
+                Long.parseLong(profileData.get("Phone")));
     }
 }
