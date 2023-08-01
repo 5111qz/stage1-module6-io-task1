2bb95b7c8bdcd2b1f883c6c4fa4bf1b6b658f84e
5111qz <139861805+5111qz@users.noreply.github.com>
2023-08-01 14:30:03 +0200

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -10,18 +10,18 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Map<String, String> profileData = new HashMap<>();
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            String rawData = "";
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            StringBuilder rawData = new StringBuilder("");
             int i = fileInputStream.read();
             while(i != -1) {
-                rawData += (char)i;
+                rawData.append((char)i);
                 i = fileInputStream.read();
             }
-            rawData = rawData.replace("\n", ": ");
-            rawData = rawData.substring(0, rawData.length() - 2);
-            fileInputStream.close();
-            String[] rawDataSplit = rawData.split(": ");
+            
+            String rawDataString = rawData.toString();
+            rawDataString = rawDataString.replace("\n", ": ");
+            rawDataString = rawDataString.substring(0, rawData.length() - 2);
+            String[] rawDataSplit = rawDataString.split(": ");
             for(i = 0; i < rawDataSplit.length - 1; i = i + 2) {
                 profileData.put(rawDataSplit[i], rawDataSplit[i+1]);
             }
